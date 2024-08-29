4d5d0b718cfd01ec658ec0c881509fe6473b6a12
aroundarmor <sourav53sd@gmail.com>
2024-08-29 18:15:57 +0530

    new-changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-southeast-1"  # Update this to your desired region
+  region = "ap-southeast-1" # Update this to your desired region
 }
 
 terraform {
--- terraform/src/lambda_function.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    message = "hey! I'm new function"
-    print(message)
-    
-    return {
-        'statusCode': 200,
-        'body': message
-    }
